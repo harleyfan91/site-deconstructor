bcaac8000a2f948d19ebc03f45587021b3c88e11
harleyfan91 <harleyfan91@aol.com>
2025-06-10 23:16:47 +0900

    Update UIAnalysisTab.tsx image analysis width

--- src/components/dashboard/UIAnalysisTab.tsx
+++ src/components/dashboard/UIAnalysisTab.tsx
@@ -76,7 +76,7 @@ const UIAnalysisTab: React.FC<UIAnalysisTabProps> = ({ data, loading, error }) =
         </Grid>
 
         {/* Image Analysis */}
-        <Grid xs={12} md={6} sx={{ display: 'flex' }}>
+        <Grid xs={12} md={6} sx={{ display: 'flex', width: '100%' }}>
           <Card sx={{ borderRadius: 2, flexGrow: 1 }}>
             <CardContent sx={{ p: 3 }}>
               <ImageAnalysisCard
